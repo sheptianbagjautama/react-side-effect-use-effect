9e0c07c45beb72535b1d2d0e8dd32fa2085007df
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-04-26 00:28:19 +0700

    using useEffect for syncing with borwser APIs

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,16 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
 function Modal({ open, children }) {
   const dialog = useRef();
 
-  dialog.current.showModal();
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, []);
 
   return createPortal(
     <dialog className="modal" ref={dialog} open={open}>
